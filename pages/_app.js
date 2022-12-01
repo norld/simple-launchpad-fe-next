b0cc53122b2496a6a838cec3638b0db3824e3fc8
Made Supardi <grymore@gmail.com>
2022-12-02 01:30:25 +0800

    initial code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { publicProvider } from "wagmi/providers/public";
+import { WagmiConfig, createClient, configureChains, chain } from "wagmi";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+
+const { chains, provider, webSocketProvider } = configureChains([chain.mainnet, chain.polygon], [publicProvider()]);
+const client = createClient({
+  autoConnect: true,
+  connectors: [new MetaMaskConnector({ chains })],
+  provider,
+  webSocketProvider,
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
